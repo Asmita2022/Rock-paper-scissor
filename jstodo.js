3fcb6a10ecad6051bfb834089c37f94fb590ae9e
Asmita2022 <117441417+Asmita2022@users.noreply.github.com>
2024-03-04 11:04:12 +0530

    Add files via upload

---
+++ jstodo.js
@@ -0,0 +1,18 @@
+let inputs = document.getElementById("task");
+let text = document.querySelector(".text");
+
+function Add() {
+    if(inputs.value == ""){
+        alert("Please enter your Task");
+    }
+    else{
+        let newElement = document.createElement("ul");
+        newElement.innerHTML = `${inputs.value} <i class="fa-solid fa-trash"></i>`;
+        text.appendChild(newElement);
+        inputs.value="";
+        newElement.querySelector("i").addEventListener("click",remove);
+        function remove() {
+            newElement.remove();
+        }
+    }
+}
